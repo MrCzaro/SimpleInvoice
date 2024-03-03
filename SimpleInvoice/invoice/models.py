381420ee0fbf93e89e656b6b9562d445af3da968
Mr.Czaro <cezary.tubacki@gmail.com>
2024-03-03 05:55:45 +0100

    first commit

---
+++ SimpleInvoice/invoice/models.py
@@ -0,0 +1,100 @@
+import uuid
+
+from django.db import models
+from django.core.validators import MinValueValidator
+from django.contrib.auth.models import User
+
+
+CURRENCY_CHOICES = (
+    ("PLN", 'Polish Złoty (PLN)'),
+    ("EUR", 'Euro (EUR)'),
+    ("GBP", 'British Pound Sterling (GBP)'),
+    ("CHF", 'Swiss Franc (CHF)'),
+    ("SEK", 'Swedish Krona (SEK)'),
+    ("NOK", 'Norwegian Krone (NOK)'),
+    ("DKK", 'Danish Krone (DKK)'),
+    ("HUF", 'Hungarian Forint (HUF)'),
+    ("CZK", 'Czech Koruna (CZK)'),
+    ("RON", 'Romanian Leu (RON)'),
+    ('USD', 'US Dollar (USD)'),
+)
+
+METHOD_PAYMENT_CHOICES = (
+    ("money transfer", "Money transfer"),
+    ("cash", "CASH"),
+)
+
+UNIT_CHOICES = (
+    ("hours", "HOURS"),
+    ("days", "DAYS"),
+    ("quantity", "QUANTITY"),
+    ("units", "UNITS"),
+    ("pieces", "PIECES"),
+)
+
+class Invoice(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # Invoice details:
+    place_of_creation = models.CharField(max_length=100)
+    date_of_creation = models.DateField()
+    date_of_service = models.DateField()
+    invoice_number = models.CharField(max_length=100)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    currency = models.CharField(max_length=50, choices=CURRENCY_CHOICES)
+    method_payment = models.CharField(max_length=20, choices=METHOD_PAYMENT_CHOICES)
+    service_type_1 = models.CharField(max_length=100)
+    service_type_2 = models.CharField(max_length=100, blank=True)
+    service_type_3 = models.CharField(max_length=100, blank=True)
+    unit_1 = models.CharField(max_length=15, choices=UNIT_CHOICES)
+    unit_2 = models.CharField(max_length=15, choices=UNIT_CHOICES, blank=True)
+    unit_3 = models.CharField(max_length=15, choices=UNIT_CHOICES, blank=True)
+    price_1 = models.DecimalField(max_digits=100, decimal_places=2, validators=[MinValueValidator(0)])
+    price_2 = models.DecimalField(max_digits=100, decimal_places=2, validators=[MinValueValidator(0)], blank=True, null=True)
+    price_3 = models.DecimalField(max_digits=100, decimal_places=2, validators=[MinValueValidator(0)], blank=True, null=True)
+    amount_1 = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
+    amount_2 = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)], blank=True, null=True)
+    amount_3 = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)], blank=True, null=True)
+    total_price_1 = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price_2 = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price_3 = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price = models.DecimalField(max_digits=20, decimal_places=2)
+    # Seller details:
+    seller_company_name = models.CharField(max_length=255)
+    seller_street_address = models.CharField(max_length=255)
+    seller_city = models.CharField(max_length=100)
+    seller_zip_code = models.CharField(max_length=50)
+    seller_tax_id = models.CharField(max_length=100)
+    seller_phone = models.CharField(max_length=20)
+    seller_account_number = models.CharField(max_length=50, blank=True)
+    # Buyer details:
+    buyer_company_name = models.CharField(max_length=255)
+    buyer_street_address = models.CharField(max_length=255)
+    buyer_city = models.CharField(max_length=100)
+    buyer_zip_code = models.CharField(max_length=50)
+    buyer_tax_id = models.CharField(max_length=100)
+
+        
+    def __str__(self):
+        return f"(Invoice Number: {self.invoice_number})"
+
+
+    def save(self, *args, **kwargs):
+        # Calculate total_price 1 which is mandatory:
+        self.total_price_1 = self.price_1 * self.amount_1
+        self.total_price = self.total_price_1
+        # Calculate total_price_2 and total_price_3 whih are not mandatory
+        if self.price_2 and self.amount_2:
+            self.total_price_2 = self.price_2 * self.amount_2
+            self.total_price += self.total_price_2
+        else:
+            self.total_price_2 = None
+        if self.price_3 and self.amount_3:
+            self.total_price_3 = self.price_3 * self.amount_3
+            self.total_price += self.total_price_3
+        else:
+            self.total_price_3 = None
+
+        # Save calculations:
+        super().save(*args, **kwargs)
